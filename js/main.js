31a97b2f7d87f4e9ba838ce87ded56afed4c919b
EEEeelijaHhHHh <elijahs.adjutovs@gmail.com>
2019-09-29 10:50:55 +0300

    Partners Section with scroll slider

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ $(function () {
   $(window).resize(function () {
     adaptive();
   })
-  
+
   adaptive();
-  
-  function adaptive () {
+
+  function adaptive() {
     prependAdaptiveItem(768, '.services__item:first-child', '.services__item:first-child .services__img');
     prependAdaptiveItem(768, '.services__item:last-child', '.services__item:last-child .services__img');
     prependAdaptiveItem(768, '.process__item:first-child', '.process__item:first-child .process__title');
@@ -43,5 +43,40 @@ $(function () {
       $(buttonGroup).find('.portfolio-menu__btn_active').removeClass('portfolio-menu__btn_active');
       $(this).addClass('portfolio-menu__btn_active');
     });
+  });
+});
+
+
+/* Partners Slider Scrolling */
+$(function () {
+  const slider = document.querySelector('.partners');
+  const speed = 1.5;
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('partners_active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
+
+  slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('partners_active');
+  });
+
+  slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('partners_active');
+  });
+
+  slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return; // stop the fn from running
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * speed;
+    slider.scrollLeft = scrollLeft - walk;
   });
 });
